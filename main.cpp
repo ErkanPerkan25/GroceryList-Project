c04c2ddb6c8c15a601f35d93ed4941ff282ddeef
Eric Hansson <erichansson.united@gmail.com>
2023-06-01 10:28:31 +0200

    inital main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**********************************************************************
+ * Author: Eric Hansson
+ * File: main.cpp
+ * Date: 31 may - _ june 2023
+ * Purpose: 
+ *
+***********************************************************************/
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    
+}
