98453dc542d51661217371774ae960fce066a265
Eric Hansson <erichansson.united@gmail.com>
2023-06-01 10:28:04 +0200

    Class to store items

---
+++ List.hpp
@@ -0,0 +1,14 @@
+#ifdef _LIST_HPP_
+#define _LIST_HPP_
+
+#include <list>
+
+class List{
+private:
+   // list of grocery items (class for grocery items)     
+public:
+
+    // Accessors
+    // Modifiers
+};
+#endif // DEBUG
