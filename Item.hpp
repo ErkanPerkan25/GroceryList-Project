8ce60ebd7387500296eef16983134bdc283b9555
Eric Hansson <erichansson.united@gmail.com>
2023-06-01 10:27:45 +0200

    Class to store data of items

---
+++ Item.hpp
@@ -0,0 +1,14 @@
+#ifndef _ITEM_HPP_
+#define _ITEM_HPP_
+
+class Item {
+private:
+    // Name of the item (ex: Carrots)
+    std::string _name;
+public:
+    // Default constructor
+    Item() : _name("") {}
+
+}
+
+#endif // !_ITEM_HPP_
