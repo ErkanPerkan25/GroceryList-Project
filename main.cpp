c5cac28e2c0921e71bfe9694d267bf137dd13e6c
Eric Hansson <erichansson.united@gmail.com>
2023-06-02 10:59:44 +0200

    Made it possible to read in items and store them in a file

--- main.cpp
+++ main.cpp
@@ -6,9 +6,30 @@
  *
 ***********************************************************************/
 #include<iostream>
+#include <fstream>
+#include "Item.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-    
+    cout << "|The Groccery list program|" << endl;
+    cout << "Enter in groccery items here: " << endl;
+
+    Item grocceryItem;
+    ofstream file("grocceryList.txt"); 
+
+    while(cin){
+        string input;
+        cin >> input;
+        if(input == "exit"){
+            break;
+        }
+        grocceryItem.getName() = input;
+        file << grocceryItem.getName() << endl;
+       
+    }
+
+    file.close();
+
+    return 0;
 }
